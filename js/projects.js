cc81df8520269684fe50bad5a3e926fd5cd609c4
alexey.kozyachiy <alexey.kozyachiy@gmail.com>
2015-08-31 01:17:00 -0400

    be able to create and edit a project

--- js/projects.js
+++ js/projects.js
@@ -2,9 +2,32 @@
  * Created by alexey.kozyachiy on 8/28/15.
  */
 (function ($) {
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+                ;
+        });
+    };
+
+    function getProjectById(id) {
+        return Projects.filter(function (p) {
+            return p.id == id;
+        })[0];
+    }
+
+    function getProjectByIndex(arr, i) {
+        return arr.filter(function (p, index) {
+            return index == i;
+        })[0];
+    }
+
     var Projects = [];
 
     function Project(p) {
+        var self = this;
         this.id = p.id;
         this.name = p.name;
         this.description = p.description;
@@ -16,12 +39,12 @@
         this.owner = {name: p.owner.name, image: p.owner.image};
         Object.defineProperty(this, 'startDate', {
             get: function () {
-                return moment.unix(this.start_date).format('ll');
+                return moment.unix(self.start_date).format('ll');
             }
         });
         Object.defineProperty(this, 'endDate', {
             get: function () {
-                return moment.unix(this.end_date).format('ll');
+                return moment.unix(self.end_date).format('ll');
             }
         });
         this.getProjectPeriod = function () {
@@ -39,21 +62,80 @@
         };
     }
 
+    function ProjectTable(data) {
+        var self = this;
+        this.projects = data;
+        this.element = $('#projectTable');
+        this.populateTable = function (addProject) {
+            this.projects.forEach(function (p) {
+                addProject(self.element.find('tbody'), p);
+            });
+            return self;
+        };
+        this.renderTableComponents = function () {
+            $('.step-bar.small').each(function (index, element) {
+                var project = getProjectByIndex(self.projects, index);
+                $(element).stepBar('create', project);
+            });
+            $('.activity').each(function (index, element) {
+                var project = getProjectByIndex(self.projects, index);
+                $(element).activity(project.active);
+            });
+            return self;
+        };
+        this.attachTableEvents = function (handler) {
+            var $body = $('body');
+            $body.on('click', 'a.link', handler)
+                .on('tr:update', 'tbody tr', function (e, projectId) {
+                    var $el = $(e.currentTarget);
+                    var p = self.getProjectById(projectId);
+                    $el.find('#' + p.id).text(p.name);
+                    $el.find('#owner').text(p.owner.name);
+                    $el.find('.activity').activity(p.active);
+                    $el.find('.step-bar.small').stepBar('update', p);
+                    $el.attr('data-visible', p.active);
+                }).on('redraw', '.activity', function (e, project) {
+                    $(this).activity(project.active);
+                }).on('redraw', '.step-bar.small', function(e, project){
+                    $(this).stepBar('create', project);
+                });
+            return self;
+        };
+    }
+
+    ProjectTable.prototype.getProjectById = function (id) {
+        return this.projects.filter(function (p) {
+            return p.id == id;
+        })[0];
+    };
 
     $(document).ready(function () {
-
-        function loadData() {
-            $.getJSON('js/challenge.json').then(function (data) {
-                data.projects.map(function (p) {
-                    this.push(new Project(p));
-                }, Projects);
+        $.getJSON('js/challenge.json').then(function (data) {
+            data.projects.map(function (p) {
+                return Projects.push(new Project(p));
             });
+            return Projects
+        }).then(function (data) {
+            attachClickEvent();
+            return new ProjectTable(data);
+        }).then(function (dataTable) {
+            dataTable.populateTable(addProject)
+                .renderTableComponents()
+                .attachTableEvents(projectClickHandler);
+            dataTable.getProjectById();
+        });
+
+        function addProject($el, p) {
+            $el.append('<tr data-visible="' + p.active + '"><td id="name"><a id=' + p.id + ' class="link">' + p.name + '</a></td><td id="owner">' + p.owner.name + '</td><td>' + p.endDate + '</td><td id="statusBar"><div class="step-bar small"></div></td><td><span class="activity status">&nbsp;</span></td></tr>');
         }
 
-        function getProjectById(id) {
-            return Projects.filter(function (p) {
-                return p.id == id;
-            })[0];
+        function projectClickHandler() {
+            var $el = $(this);
+            var id = $el.attr('id');
+            $('#itemView').show(1, function () {
+                populateItemViewWthData($(this), getProjectById(id));
+            });
+            $('#listView').hide();
         }
 
         function populateItemViewWthData(view, p) {
@@ -73,6 +155,7 @@
             view.find('#projectOwner').text(p.owner.name);
             view.find('#projectRation').text(p.getRation());
             view.find('#projectProgress').css('width', p.getPercentComplete());
+            view.find('#editProjectButton').attr('data-project', p.id);
 
         }
 
@@ -80,16 +163,28 @@
             return $(parent).find(selector).val();
         }
 
+        function setElementValue(parent, selector, value) {
+            $(parent).find(selector).val(value).end();
+        }
+
+        function applyFilterToProjects(status) {
+            $('tbody>tr').show();
+            switch (status) {
+                case "2":
+                    $('tbody>tr[data-visible="false"]').toggle();
+                    break;
+                case "3":
+                    $('tbody>tr[data-visible="true"]').toggle();
+                    break;
+                default :
+                    $('tbody>tr').show();
+                    break;
+            }
+        }
+
         function attachClickEvent() {
-            $("#name>a").on('click', function () {
-                var $el = $(this);
-                var id = $el.attr('id');
-                $('#itemView').show(1, function () {
-                    populateItemViewWthData($(this), getProjectById(id));
-                });
-                $('#listView').hide();
-            });
             $('#toListView').on('click', function () {
+                var $table = $('#projectTable');
                 $('#itemView').hide();
                 $('#listView').show();
             });
@@ -98,26 +193,65 @@
                 var id = $el.find('span').text();
                 populateItemViewWthData($('#itemView'), getProjectById(id));
             });
-            $('#newProjectButton').on('click', function () {
-                var $modal = $('#myModal');
-                $modal.find('.modal-title').text('Add New Project');
-                $modal.modal();
-            });
-            $('#myModal').on('shown.bs.modal', function () {
-                console.log('Modal Here');
-            }).on('show.bs.modal', function () {
-                var form = $(this).find('#form');
-                console.log(form);
+            $('#myModal').on('show.bs.modal', function (e) {
+                var $trigger = $(e.relatedTarget);
+                var $modal = $(e.currentTarget);
+                var title = $trigger.data('title');
+                var projectId = $trigger.attr('data-project');
+                $modal.find('.modal-title').text(title);
+                if (projectId) {
+                    var p = getProjectById(projectId);
+                    setElementValue($modal, '#projectIdForm', p.id);
+                    setElementValue($modal, '#projectNameForm', p.name);
+                    setElementValue($modal, '#projectOwnerForm', p.owner.name);
+                    setElementValue($modal, '#projectDescriptionForm', p.description);
+                    if (p.active) {
+                        $modal.find('#projectActiveForm').prop('checked', true);
+                    }
+                } else {
+                    setElementValue($modal, '#projectIdForm', Projects.length + 1);
+                }
+            }).on('hidden.bs.modal', function (e) {
+                var $modal = $(e.currentTarget);
+                setElementValue($modal, '#projectIdForm', '');
+                setElementValue($modal, '#projectNameForm', '');
+                setElementValue($modal, '#projectOwnerForm', '');
+                setElementValue($modal, '#projectDescriptionForm', '');
+                $modal.find('#projectActiveForm').prop('checked', '');
+
+            }).on('shown.bs.modal', function (e) {
+                var $modal = $(e.currentTarget);
+                $modal.find('#projectNameForm').focus();
             });
             $('#form').on('submit', function (e) {
-                var projectName = getElementValue(this, '#projectNameForm');
-                var projectOwner = getElementValue(this, '#projectOwnerForm');
-                var startDateForm = getElementValue(this, '#startDateForm');
-                var endDateForm = getElementValue(this, '#endDateForm');
-                var projectDescriptionForm = getElementValue(this, '#projectDescriptionForm');
-                var projectActiveForm = $(this).find('#projectActiveForm').is(":checked");
+                var p = {
+                    id: getElementValue(this, '#projectIdForm'),
+                    name: getElementValue(this, '#projectNameForm'),
+                    owner: {name: getElementValue(this, '#projectOwnerForm')},
+                    description: getElementValue(this, '#projectDescriptionForm'),
+                    active: $(this).find('#projectActiveForm').is(":checked")
+                };
+                var project = getProjectById(p.id);
+                var $table = $('#projectTable').find('tbody');
+                if (project) {
+                    project.name = p.name;
+                    project.description = p.description;
+                    project.active = p.active;
+                    project.owner.name = p.owner.name;
+                    populateItemViewWthData($('#itemView'), project);
+                    $table.find('tr').eq(project.id - 1).trigger('tr:update', p.id);
 
-                    console.log(projectName, projectOwner, projectDescriptionForm, projectActiveForm);
+                } else {
+                    p['current_step'] = 10;
+                    p['total_steps'] = 18;
+                    p['start_date']= moment(new Date()).format('X');
+                    p['end_date'] = moment(new Date()).add(5, 'days').format('X')
+                    var newProject = new Project(p);
+                    Projects.push(newProject);
+                    addProject($table, newProject);
+                    $table.find('.activity').eq(p.id - 1).trigger('redraw', newProject);
+                    $table.find('.step-bar.small').eq(p.id - 1).trigger('redraw', newProject);
+                }
                 e.preventDefault();
             });
 
@@ -127,17 +261,72 @@
             });
         }
 
-        function populateTable($el, data) {
-            data.forEach(function (p) {
-                $el.append('<tr><td id="name"><a id=' + p.id + ' class="link">' + p.name + '</a></td><td>' + p.owner.name + '</td><td>' + p.endDate + '</td><td id="statusBar">StepBar Here</td><td id="active">' + p.active + '</td></tr>');
-            });
-            attachClickEvent();
-        }
-
-        loadData();
-        setTimeout(function () {
-            populateTable($('.table>tbody'), Projects);
-        }, 500);
+        $('#project-filter').filters({
+            filters: [{
+                text: 'View All',
+                value: 1
+            }, {
+                text: 'Active',
+                value: 2
+            }, {
+                text: 'Inactive',
+                value: 3
+            }],
+            onFilter: function (text) {
+                applyFilterToProjects(text);
+            }
+        });
     });
 
+})(jQuery);
+
+(function ($) {
+    $.fn.stepBar = function (event, p) {
+        var isActive = p.active;
+        if (event == 'create') {
+            var str = '<span class="total">{0}</span><span class="current">{1}</span><div class="progress"> <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: {2}%"></div></div>'.format(p.total_steps, p.current_step, p.getPercentComplete());
+            this.append(str);
+        }
+        if (!isActive) {
+            this.find('.total').hide();
+            this.find('.current').addClass('active');
+            this.find('.progress-bar').hide();
+        } else {
+            this.find('.total').show();
+            this.find('.current').removeClass('active');
+            this.find('.progress-bar').show();
+        }
+        return this;
+    }
+})(jQuery);
+
+(function ($) {
+    $.fn.activity = function (state) {
+        if (state) {
+            this.addClass('active');
+        } else if (this.hasClass('active')) {
+            this.removeClass('active');
+        }
+        return this;
+    }
+})(jQuery);
+
+(function ($) {
+    $.fn.filters = function (options) {
+        var self = this;
+        var buttons = options.filters;
+        var onFilterFn = options.onFilter;
+        buttons.forEach(function (b) {
+            var innerHTML = '<lable class="btn btnc btn-custom"><input value="' + b.value + '" type="radio" name="radioFilter"/>' + b.text + '</lable>';
+            self.append(innerHTML);
+        });
+        this.find('lable').eq(0).addClass('active');
+        this.find("input[type='radio']").on('change', function () {
+            var thisCheck = $(this);
+            if (thisCheck.is(':checked')) {
+                onFilterFn(thisCheck.val());
+            }
+        });
+        return this;
+    }
 })(jQuery);
